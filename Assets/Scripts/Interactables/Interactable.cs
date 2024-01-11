a21c526ea4e912aef4edb97048ab3254fc0cff9a
jymyte <jyrimy@gmail.com>
2024-01-12 01:12:22 +0200

    EventOnlyInteractable to make gameobjects interactable with unityEvents.
     - Some custom editor stuff to make this easier.

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public abstract class Interactable : MonoBehaviour
 {
+  //Add or remove an InteractionEvent component on this gameobject.
+  public bool  useEvents;
+  [SerializeField]
   public string promptMessage;
 
+  
+
   public void BaseInteract() {
+    if (useEvents)
+      GetComponent<InteractionEvent>().OnInteract.Invoke();
     Interact();
   }
   protected virtual void Interact() {
